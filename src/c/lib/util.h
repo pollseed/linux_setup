615027d665aa5f49f600b9f585f27e4403ec6135
pollseed <cb.so.devope+0001@gmail.com>
2015-10-09 13:20:14 +0900

    Create util.h

---
+++ src/c/lib/util.h
@@ -0,0 +1,10 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
+void ln(char str[]);
+void func(int *pvalue);
+int average(int *data, int size);
+int read(char *name, char *permission);
+void write(char *name, char *permission, char *body);
+
+#endif
