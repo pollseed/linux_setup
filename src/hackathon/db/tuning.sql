b9ee51637d38141590a18c169697abca8fe91183
pollseed <cb.so.devope+0001@gmail.com>
2015-10-10 06:40:34 +0900

    Create tuning.sql

---
+++ src/hackathon/db/tuning.sql
@@ -0,0 +1,51 @@
+-- 1.
+-- item テーブル
+CREATE TABLE `item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `pref_code` int(11) NOT NULL,
+  `items_code` int(11) NOT NULL,
+  `classify_code` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+-- items テーブル
+CREATE TABLE `items` (
+  `items_code` int(11) NOT NULL AUTO_INCREMENT,
+  `items_name` varchar(128) NOT NULL,
+  PRIMARY KEY (`items_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8
+
+-- 2.
+-- NON Tuning
+explain 
+SELECT *
+FROM item i 
+INNER JOIN pref p ON i.pref_code = p.pref_code 
+WHERE p.pref_code = 1
+
+-- Add Index
+ALTER TABLE `item` 
+ADD INDEX `idx_item1` USING BTREE (`pref_code` ASC);
+
+-- Add FK
+ALTER TABLE `test`.`item` 
+ADD CONSTRAINT `fk_item1`
+  FOREIGN KEY (`pref_code`)
+  REFERENCES `test`.`pref` (`pref_code`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+  
+-- Select Count
+SELECT 
+    COUNT(*), COUNT(IF(pref_code = 1, 1, NULL))
+FROM
+    test.item;
+
+SELECT * FROM item WHERE pref_code <> 1;
+
+-- Tuning
+explain 
+SELECT *
+FROM item i 
+INNER JOIN pref p ON i.pref_code = p.pref_code
+WHERE p.pref_code = 3;
