d29cf731700d7046cb7264da7ab17932680ed5e9
pollseed <cb.so.devope+0001@gmail.com>
2015-10-09 13:19:10 +0900

    Update util.c

--- src/c/lib/util.c
+++ src/c/lib/util.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "util.h"
 
 int average(int *data, int size);
 void write(char *name, char *permission, char *body);
@@ -8,27 +9,4 @@ int main(void)
   int arr[] = {3,5,324,5,12,5,32,21};
   const size = sizeof arr/sizeof(int);
   printf("%d\n", average(arr, size);
-}
-
-int average(int *data, int size)
-{
-  if (data == NULL || size == 0) {
-    return 0;
-  }
-  int i, average = 0;
-  for (i = 0; i < size; i++) {
-    average += data[i];
-  }
-  return average / size;
-}
-
-void write(char *name, char *permission, char *body)
-{
-  if (name == NULL || permission == NULL || body == NULL) {
-    return;
-  }
-  FILE *file;
-  file = fopen(name, permission);
-  fprintf(file, body);
-  fclose(file);
 }
