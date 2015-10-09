5485fdfe171f43e1030bd3e86b4b4ca242bb17c4
pollseed <cb.so.devope+0001@gmail.com>
2015-10-09 13:21:10 +0900

    Update main.c

--- src/c/lib/main.c
+++ src/c/lib/main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include "util.h"
 
-int average(int *data, int size);
-void write(char *name, char *permission, char *body);
-
 int main(void)
 {
-  int arr[] = {3,5,324,5,12,5,32,21};
-  const size = sizeof arr/sizeof(int);
-  printf("%d\n", average(arr, size);
+    ln("hello, world");
+    int *p,i,value=10, ave, array[] = {1,4,2,4,4}, buf;
+    const array_size = sizeof array/sizeof(int);
+    p = &i;
+    *p = 10;
+    printf("p = %p\n", p);
+    printf("*p = %p\n", *p);
+    printf("i = %p\n", i);
+    printf("&i = %p\n", &i);
+    printf("&value = %p\n", &value);
+    func(&value);
+    printf("value = %d\n", value);
+    ave = average(array, array_size);
+    ave = average(NULL, array_size);
+    printf("%d\n", ave);
+    write("text.txt", "w", "hewwo, world");
+    buf = read("text.txt", "rb");
+    printf("%d\n", buf);
+    return 0;
 }
